74d8eaa9acd433f55766f60817f7503bba4b40e8
jiangyusheng <jiangyusheng@WINDOWS-FPIDAS5>
2019-03-14 15:33:29 +0800

    448 解决 ATRS 001 week

---
+++ src/LeetCode/P448.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	nums := []int{4, 3, 2, 7, 8, 2, 3, 1}
+	findDisappearedNumbers(nums)
+}
+
+func findDisappearedNumbers(nums []int) []int {
+	if nums == nil || len(nums) == 0 {
+		return []int{}
+	}
+	var res []int
+	for i := 0; i < len(nums); i++ {
+		value := nums[i]
+		for nums[i] != i+1 && nums[value-1] != nums[i] {
+			nums[i], nums[value-1] = nums[value-1], nums[i]
+			value = nums[i]
+		}
+	}
+	for i := range nums {
+		if i+1 != nums[i] {
+			res = append(res, i+1)
+		}
+	}
+	return res
+}
